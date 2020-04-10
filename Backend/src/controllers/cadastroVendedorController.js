16dfd103c9d525cc518927bcab89f98b39bf6d83
wjulio <wolgranjulio@gmail.com>
2020-04-09 21:10:35 -0300

    Controller vendedor e inserindo vendedor no banco

---
+++ Backend/src/controllers/cadastroVendedorController.js
@@ -0,0 +1,28 @@
+const connection = require('../database/conection');
+module.exports = {
+    async create(request, response){
+    // acessando corpo da requisição
+    const {name,
+	password,
+	cpf,
+	rg,
+	email,
+	whatsapp,
+	city,
+	uf
+    } = request.body;
+    await connection('vendedor').insert({
+        name,
+	password,
+	cpf,
+	rg,
+	email,
+	whatsapp,
+	city,
+	uf
+    })
+    //aqui serve pra retornar alguma coisa
+    return response.json({})
+
+    }
+};
--- Backend/src/routes.js
+++ Backend/src/routes.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const routes = express.Router();
 
+const cadastroVendedorController = require('./controllers/cadastroVendedorController');
+
+routes.post('/cadastroVendedorController', cadastroVendedorController.create);
+
+
+
+
+
+
+
+
+
 
 
 module.exports = routes;
