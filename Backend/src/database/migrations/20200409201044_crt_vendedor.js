322e3dc83a34650aa0390975d7d3288713a23aad
RonniSouza <ronnisouza21@gmail.com>
2020-04-10 10:09:41 -0300

    Atualização das tabelas(vendedor,produto)

--- Backend/src/database/migrations/20200409201044_crt_vendedor.js
+++ Backend/src/database/migrations/20200409201044_crt_vendedor.js
@@ -1,13 +1,13 @@
 
 exports.up = function(knex) {
     return knex.schema.createTable('vendedor', function (table) {
-        table.increments();//chave primaria autoincrement
+        table.string('id').primary();
         table.string('name').notNullable();
         table.string('password').notNullable();
         table.integer('cpf').notNullable();
         table.integer('rg').notNullable();
         table.string('email').notNullable();
-        table.integer('whatsapp');
+        table.integer('whatsapp').notNullable();
         table.string('city').notNullable();
         table.string('uf', 2).notNullable();
       });
--- Backend/src/database/migrations/20200409203011_crt_produto.js
+++ Backend/src/database/migrations/20200409203011_crt_produto.js
@@ -5,6 +5,10 @@ exports.up = function(knex) {
        table.string('name').notNullable();
        table.string('description').notNullable();;
        table.decimal('value').notNullable();
+
+       table.string('vendedor_id').notNullable();
+
+       table.foreign('vendedor_id').references('id').inTable('vendedor');
     });
 };
 
