cdf9a7810a7500a511408c845073a4b47a898c83
RonniSouza <ronnisouza21@gmail.com>
2020-04-10 09:50:51 -0300

    completed product table

--- Backend/src/database/migrations/20200409203011_crt_produto.js
+++ Backend/src/database/migrations/20200409203011_crt_produto.js
@@ -1,8 +1,13 @@
 
 exports.up = function(knex) {
-  
+   return knex.schema.createTable('product', function(table) {
+       table.increments();
+       table.string('name').notNullable();
+       table.string('description').notNullable();;
+       table.decimal('value').notNullable();
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('product');
 };
