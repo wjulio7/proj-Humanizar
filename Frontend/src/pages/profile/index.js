6cebf82b6fb35fe6ea4c2a69b7bb715768803347
wjulio <wolgranjulio@gmail.com>
2020-05-06 16:45:51 -0300

    Listagem de produtos na pagina Profile

--- Frontend/src/pages/profile/index.js
+++ Frontend/src/pages/profile/index.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {Link, useHistory} from 'react-router-dom'
 import {FiPower, FiTrash2} from "react-icons/fi";
 
@@ -9,8 +9,20 @@ export default function Profile() {
 
     const userCpf = localStorage.getItem('userCpf')
     const userName = localStorage.getItem('userName')
+    const [produtos, setProdutos] = useState([]);
 
     const history = useHistory()
+
+    useEffect(() => {
+        api.get('produtoslistar',{
+            headers:{
+                Authorization: userCpf,
+            }
+        }).then(response => {
+            setProdutos(response.data);
+        })
+    }, [userCpf]);//vazio executa uma unica vez
+
 
     function handleLogout() {
         localStorage.clear()
@@ -27,6 +39,24 @@ export default function Profile() {
                 <FiPower  size={18} color="#E02041"/>
             </button>
 
+            <ul>
+                {produtos.map(produto => (
+                    <li key={produto.id}>
+                        <strong>CASO:</strong>
+                        <p>{produto.title}</p>
+                        <strong>DESCRIÇÃO</strong>
+                        <p>{produto.description}</p>
+                        <strong>VALOR</strong>
+                        <p>{Intl.NumberFormat('pt-BR', {style: 'currency', currency: 'BRL'}).format(produto.value)}</p>
+                        <strong>Contato do Vendedor</strong>
+                        <p>{produto.email}</p>
+                        <p>{produto.whatsapp}</p>
+                        <p>{produto.city}</p>
+                        <p>{produto.uf}</p>
+
+                    </li>
+                ))}
+            </ul>
 
         </div>
     )
