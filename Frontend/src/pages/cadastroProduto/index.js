f84e438ab0ca1f318aed8011ecc01916ae2080d4
tuliusalves <tulius_sr@outlook.com>
2020-05-07 17:08:03 -0300

    Mudança do nome da pasta
    -Pasta Profile foi alterada para cadastroProduto

---
+++ Frontend/src/pages/cadastroProduto/index.js
@@ -0,0 +1,20 @@
+import React,{useState} from 'react'
+import {Link, useHistory} from 'react-router-dom'
+
+import api from '../../services/api'
+import './styles.css'
+
+export default function Profile() {
+
+    return(
+
+        <div>
+            <h1>profile</h1>
+        </div>
+
+
+
+
+
+    )
+}
